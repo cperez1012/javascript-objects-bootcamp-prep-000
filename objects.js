1586fa3c378e2c88652e0fa2e2da151aac651dfe
Christian Perez <perezchristian1012@gmail.com>
2019-06-20 18:29:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,3 +6,9 @@ function updatePlaylist (playlist, artistName, songTitle){
   var playlist = { [artistName]:songTitle }
   return playlist
 }
+
+function removeFromPlaylist (playlist, artistName){
+
+  delete playlist [artistName]
+  return playlist
+}
